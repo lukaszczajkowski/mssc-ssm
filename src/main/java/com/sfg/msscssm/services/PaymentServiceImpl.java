de49c3acce8015dd50d93025c1a52384aedab178
Lukasz Czajkowski <lukaszczajkowski.wroc@gmail.com>
2021-03-20 16:34:31 +0100

    Sending events implemented

---
+++ src/main/java/com/sfg/msscssm/services/PaymentServiceImpl.java
@@ -0,0 +1,82 @@
+package com.sfg.msscssm.services;
+
+import com.sfg.msscssm.domain.Payment;
+import com.sfg.msscssm.domain.PaymentEvent;
+import com.sfg.msscssm.domain.PaymentState;
+import com.sfg.msscssm.repository.PaymentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.config.StateMachineFactory;
+import org.springframework.statemachine.support.DefaultStateMachineContext;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class PaymentServiceImpl implements PaymentService {
+
+    private static final String PAYMENT_ID_HEADER = "payment_id";
+
+    private final PaymentRepository paymentRepository;
+    private final StateMachineFactory<PaymentState, PaymentEvent> stateMachineFactory;
+
+    @Override
+    public Payment newPayment(Payment payment) {
+        payment.setState(PaymentState.NEW);
+        return paymentRepository.save(payment);
+    }
+
+    @Override
+    public StateMachine<PaymentState, PaymentEvent> preAuth(Long paymentId) {
+        StateMachine<PaymentState, PaymentEvent> stateMachine = build(paymentId);
+
+        sendEvent(paymentId, stateMachine, PaymentEvent.PRE_AUTHORIZE);
+
+        return null;
+    }
+
+    @Override
+    public StateMachine<PaymentState, PaymentEvent> authorizePayment(Long paymentId) {
+        StateMachine<PaymentState, PaymentEvent> stateMachine = build(paymentId);
+
+        sendEvent(paymentId, stateMachine, PaymentEvent.AUTH_APPROVED);
+
+        return null;
+    }
+
+    @Override
+    public StateMachine<PaymentState, PaymentEvent> declineAuth(Long paymentId) {
+        StateMachine<PaymentState, PaymentEvent> stateMachine = build(paymentId);
+
+        sendEvent(paymentId, stateMachine, PaymentEvent.AUTH_DECLINED);
+
+        return null;
+    }
+
+    private void sendEvent(Long paymentId, StateMachine<PaymentState, PaymentEvent> stateMachine, PaymentEvent event) {
+        Message msg = MessageBuilder.withPayload(event)
+                .setHeader(PAYMENT_ID_HEADER, paymentId)
+                .build();
+
+        stateMachine.sendEvent(msg);
+    }
+
+    private StateMachine<PaymentState, PaymentEvent> build(Long paymentId) {
+        Payment payment = paymentRepository.getOne(paymentId);
+
+        StateMachine<PaymentState, PaymentEvent> stateMachine =
+                stateMachineFactory.getStateMachine(Long.toString(payment.getId()));
+
+        stateMachine.stop();
+
+        stateMachine.getStateMachineAccessor()
+                .doWithAllRegions(sma -> {
+                    sma.resetStateMachine(new DefaultStateMachineContext<>(payment.getState(), null, null, null));
+                });
+
+        stateMachine.start();
+
+        return stateMachine;
+    }
+}
